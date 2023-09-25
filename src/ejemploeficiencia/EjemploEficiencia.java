4a48445b07a762a530c8d486a9465e8a54caac63
Piruxb17 <Piruxb17@192.168.1.45>
2023-09-25 03:12:06 -0300

    subiendo

---
+++ src/ejemploeficiencia/EjemploEficiencia.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemploeficiencia;
+
+/**
+ *
+ * @author Piruxb17
+ */
+public class EjemploEficiencia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AgregarPersonaError vAgregar= new AgregarPersonaError();
+            vAgregar.setVisible(true);
+        /*AgregarPersonaExcepciones vAgregar= new AgregarPersonaExcepciones();
+            vAgregar.setVisible(true);*/
+      /*AgregarPersonaRegex vAgregar= new AgregarPersonaRegex();
+            vAgregar.setVisible(true);*/
+    }
+    
+}
